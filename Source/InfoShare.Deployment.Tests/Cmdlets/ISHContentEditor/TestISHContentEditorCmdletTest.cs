4e34317c998e7e2c8010b4ed1691038319611ed0
Orlov Oleksandr <oorlov@sdl.com>
2016-02-05 15:02:32 +0200

    Added simple test. More to come.

---
+++ Source/InfoShare.Deployment.Tests/Cmdlets/ISHContentEditor/TestISHContentEditorCmdletTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Security.AccessControl;
+using InfoShare.Deployment.Cmdlets.ISHContentEditor;
+using InfoShare.Deployment.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InfoShare.Deployment.Tests.Cmdlets.ISHContentEditor
+{
+    [TestClass]
+    public class TestISHContentEditorCmdletTest : BaseTest
+    {
+        public TestISHContentEditorCmdletTest()
+        {
+
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+
+        }
+
+		[TestMethod]
+		[TestCategory("Cmdlets")]
+		public void ProcessRecord()
+		{
+			var cmdlet = new TestISHContentEditorCmdlet
+			{
+				Hostname = "localhost",
+				IshProject = this.IshProject
+			};
+
+			var result = cmdlet.Invoke();
+
+			foreach (var item in result)
+			{
+				bool isValid;
+				Assert.IsTrue(Boolean.TryParse(item.ToString(), out isValid), "Result must be a boolean");
+			}
+		}
+	}
+}

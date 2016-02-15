da2a878371710444ca1f58b542aa65d9217ef5a4
Volodymyr Maksymenko <vmaksymenko@sdl.com>
2016-02-15 18:20:09 +0200

    Minor change

--- Source/InfoShare.Deployment/Business/ISHPaths.cs
+++ Source/InfoShare.Deployment/Business/ISHPaths.cs
@@ -7,23 +7,23 @@ namespace InfoShare.Deployment.Business
     {
         #region Enable/Disable Content Editor (XOPUS)
 
-        public string FolderButtonbar { get { return GetPath(@"Author\ASP\XSL\FolderButtonbar.xml"); } }
-        public string InboxButtonBar { get { return GetPath(@"Author\ASP\XSL\InboxButtonBar.xml"); } }
-        public string LanguageDocumentButtonBar { get { return GetPath(@"Author\ASP\XSL\LanguageDocumentButtonbar.xml"); } }
-        public string LicenceFolderPath { get { return GetPath(@"Author\ASP\Editors\Xopus\license\"); } }
+        public string FolderButtonbar { get { return CombineAuthorFolderPath(@"Author\ASP\XSL\FolderButtonbar.xml"); } }
+        public string InboxButtonBar { get { return CombineAuthorFolderPath(@"Author\ASP\XSL\InboxButtonBar.xml"); } }
+        public string LanguageDocumentButtonBar { get { return CombineAuthorFolderPath(@"Author\ASP\XSL\LanguageDocumentButtonbar.xml"); } }
+        public string LicenceFolderPath { get { return CombineAuthorFolderPath(@"Author\ASP\Editors\Xopus\license\"); } }
 
         #endregion
 
         #region Enable/Disable Enrich
 
-        public string EnrichConfig { get { return GetPath(@"Author\ASP\Editors\Xopus\config\bluelion-config.xml"); } }
-		public string XopusConfig { get { return GetPath(@"Author\ASP\Editors\Xopus\config\config.xml"); } }
+        public string EnrichConfig { get { return CombineAuthorFolderPath(@"Author\ASP\Editors\Xopus\config\bluelion-config.xml"); } }
+		public string XopusConfig { get { return CombineAuthorFolderPath(@"Author\ASP\Editors\Xopus\config\config.xml"); } }
 
 		#endregion
         
         #region Enable/Disable ExternalPreview
 
-        public string AuthorAspWebConfig { get { return GetPath(@"Author\ASP\Web.config"); } }
+        public string AuthorAspWebConfig { get { return CombineAuthorFolderPath(@"Author\ASP\Web.config"); } }
 
         #endregion
 
@@ -33,7 +33,7 @@ namespace InfoShare.Deployment.Business
             _ishDeployment = ishDeployment;
         }
 
-        private string GetPath(string relativePath)
+        private string CombineAuthorFolderPath(string relativePath)
         {
             return Path.Combine(_ishDeployment.AuthorFolderPath, relativePath);
         }

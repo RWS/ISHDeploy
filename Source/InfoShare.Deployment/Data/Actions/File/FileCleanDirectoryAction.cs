3514554802d5f06f66356942b86e826c9c6ccebd
Orlov Oleksandr <oorlov@sdl.com>
2016-02-29 15:23:00 +0200

    Undo action is split now to Clean-Folder and Copy-Folder-Content

---
+++ Source/InfoShare.Deployment/Data/Actions/File/FileCleanDirectoryAction.cs
@@ -0,0 +1,34 @@
+﻿using InfoShare.Deployment.Data.Managers.Interfaces;
+using InfoShare.Deployment.Interfaces;
+
+namespace InfoShare.Deployment.Data.Actions.File
+{
+	/// <summary>
+	/// Action responcible for rollong back changes to vanilla state
+	/// </summary>
+    public class FileCleanDirectoryAction : BaseAction
+	{
+        private readonly IFileManager _fileManager;
+        private readonly string _folder;
+
+		/// <summary>
+		/// Reverts changes to vanilla state. If backup folder is not defined, then we do remove all from deployment folder
+		/// </summary>
+		/// <param name="logger">Logger Object</param>
+		/// <param name="folderPath">Folder at deployment, where to put the reverted changes</param>
+		public FileCleanDirectoryAction(ILogger logger, string folderPath)
+			: base(logger)
+		{
+			_fileManager = ObjectFactory.GetInstance<IFileManager>();
+			_folder = folderPath;
+		}
+
+		/// <summary>
+		/// And cleans up the folder
+		/// </summary>
+		public override void Execute()
+		{
+			_fileManager.CleanFolder(_folder);
+		}
+	}
+}
---
+++ Source/InfoShare.Deployment/Data/Actions/File/FileCopyDirectoryAction.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Linq;
+using InfoShare.Deployment.Data.Managers.Interfaces;
+using InfoShare.Deployment.Interfaces;
+
+namespace InfoShare.Deployment.Data.Actions.File
+{
+	/// <summary>
+	/// Action responcible for rollong back changes to vanilla state
+	/// </summary>
+    public class FileCopyDirectoryAction : BaseAction
+	{
+        private readonly IFileManager _fileManager;
+        private readonly string _sourceFolder;
+        private readonly string _destinationFolder;
+
+
+		/// <summary>
+		/// Reverts changes to vanilla state
+		/// </summary>
+		/// <param name="logger">Logger object</param>
+		/// <param name="sourceFolder">Folder which source content</param>
+		/// <param name="destinationFolder">Destination folder</param>
+		public FileCopyDirectoryAction(ILogger logger, string sourceFolder, string destinationFolder)
+            : base(logger)
+        {
+			_sourceFolder = sourceFolder;
+			_destinationFolder = destinationFolder;
+
+			_fileManager = ObjectFactory.GetInstance<IFileManager>();
+		}
+
+		/// <summary>
+		/// Copies file from source folder to destination folder.
+		/// </summary>
+		public override void Execute()
+	    {
+			_fileManager.CopyDirectoryContent(_sourceFolder, _destinationFolder);
+		}
+	}
+}

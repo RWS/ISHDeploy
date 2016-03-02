e418f84cb258f2e2860270fa1b8edb06dfcab6d2
Orlov Oleksandr <oorlov@sdl.com>
2016-03-02 09:28:44 +0200

    Fixed -Deployment to -ISHDeployment input parameter

--- Source/InfoShare.Deployment/Cmdlets/ISHDeployment/UndoISHDeploymentCmdlet.cs
+++ Source/InfoShare.Deployment/Cmdlets/ISHDeployment/UndoISHDeploymentCmdlet.cs
@@ -1,24 +1,27 @@
 ﻿using System.Management.Automation;
 using InfoShare.Deployment.Business.Operations.ISHDeployment;
+using InfoShare.Deployment.Providers;
 
 namespace InfoShare.Deployment.Cmdlets.ISHDeployment
 {
     [Cmdlet(VerbsCommon.Undo, "ISHDeployment")]
     public class UndoISHDeploymentCmdlet : BaseCmdlet
     {
-        /// <summary>
-        /// Deployment prefix to identify deployment to be rolled back
-        /// </summary>
-        [Parameter(Mandatory = false, Position = 0, HelpMessage = "Suffix of the already deployed Content Manager instance")]
-        [Alias("Suffix")]
-        public string Deployment { get; set; }
+		/// <summary>
+		/// Ish deployment parameter, to identify deployment to be rolled back
+		/// </summary>
+		[Parameter(Mandatory = false, Position = 0, HelpMessage = "Already deployed Content Manager instance, to be rolled back")]
+		[Alias("proj")]
+		[ValidateNotNull]
+		public Models.ISHDeployment ISHDeployment { get; set; }
 
 		/// <summary>
 		/// Executes revert changes cmdLet
 		/// </summary>
         public override void ExecuteCmdlet()
-        {
-			var cmdSet = new UndoISHDeploymentOperation(Logger, Deployment);
+		{
+			Models.ISHDeployment deployment = ISHDeployment ?? ISHProjectProvider.Instance.ISHDeployment;
+			var cmdSet = new UndoISHDeploymentOperation(Logger, deployment);
 			cmdSet.Run();
 		}
     }

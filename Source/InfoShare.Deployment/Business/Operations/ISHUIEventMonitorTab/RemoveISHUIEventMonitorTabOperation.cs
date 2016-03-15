157a49aaf3e0beaa394f64ebe4e1b5fb71d77eae
Orlov Oleksandr <oorlov@sdl.com>
2016-03-15 18:40:54 +0200

    Initial version.

---
+++ Source/InfoShare.Deployment/Business/Operations/ISHUIEventMonitorTab/RemoveISHUIEventMonitorTabOperation.cs
@@ -0,0 +1,39 @@
+﻿using InfoShare.Deployment.Business.Invokers;
+using InfoShare.Deployment.Data.Actions.XmlFile;
+using InfoShare.Deployment.Interfaces;
+
+namespace InfoShare.Deployment.Business.Operations.ISHUIEventMonitorTab
+{
+	/// <summary>
+	/// Removes Event Monitor Tab".
+	/// </summary>
+	/// <seealso cref="IOperation" />
+	public class RemoveISHUIEventMonitorTabOperation : IOperation
+    {
+        /// <summary>
+        /// The actions invoker
+        /// </summary>
+        private readonly IActionInvoker _invoker;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="RemoveISHUIEventMonitorTabOperation"/> class.
+		/// </summary>
+		/// <param name="logger">The logger.</param>
+		/// <param name="paths">Reference for all files paths.</param>
+		/// <param name="label">Label of the element</param>
+		public RemoveISHUIEventMonitorTabOperation(ILogger logger, ISHPaths paths, string label)
+        {
+            _invoker = new ActionInvoker(logger, "Removing Event Monitor Tab");
+            
+            _invoker.AddAction(new RemoveSingleNodeAction(logger, paths.EventMonitorMenuBar, CommentPatterns.XopusAddCheckOut));
+        }
+
+        /// <summary>
+        /// Runs current operation.
+        /// </summary>
+        public void Run()
+        {
+            _invoker.Invoke();
+        }
+    }
+}
---
+++ Source/InfoShare.Deployment/Data/Actions/XmlFile/RemoveSingleNodeAction.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using InfoShare.Deployment.Interfaces;
+using InfoShare.Deployment.Models;
+
+namespace InfoShare.Deployment.Data.Actions.XmlFile
+{
+    /// <summary>
+    /// Action that removes single node in xml file found by xpaths.
+    /// </summary>
+    /// <seealso cref="SingleXmlFileAction" />
+    public class RemoveSingleNodeAction : SingleXmlFileAction
+    {
+        /// <summary>
+        /// The xpath to the xml node.
+        /// </summary>
+        private readonly string _xpath;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="RemoveSingleNodeAction"/> class.
+		/// </summary>
+		/// <param name="logger">The logger.</param>
+		/// <param name="filePath">The xml file path.</param>
+		/// <param name="xpath">The xpath to the node that needs to be removed.</param>
+		public RemoveSingleNodeAction(ILogger logger, ISHFilePath filePath, string xpath)
+			: base(logger, filePath)
+        {
+            _xpath = xpath;
+        }
+
+        /// <summary>
+        /// Executes current action.
+        /// </summary>
+        public override void Execute()
+        {
+			XmlConfigManager.RemoveSingleNode(FilePath, _xpath);
+        }
+    }
+}

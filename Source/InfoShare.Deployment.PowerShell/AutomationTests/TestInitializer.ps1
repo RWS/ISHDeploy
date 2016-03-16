3488bf8b94d41d6c18f67328a5e68fea0452822f
Denys Havryliuk <dhavryliuk@sdl.com>
2016-03-16 15:05:37 +0200

    Tests are changed to new Infoshare.Deployment model and added to TestRunner script

--- Source/InfoShare.Deployment.PowerShell/AutomationTests/TestInitializer.ps1
+++ Source/InfoShare.Deployment.PowerShell/AutomationTests/TestInitializer.ps1
@@ -8,6 +8,7 @@ $ISHUIExternalPreview = Join-Path $executingScriptDirectory "ISHUIExternalPrevie
 $GETISHDeployment = Join-Path $executingScriptDirectory "GET-ISHDeployment.ps1"
 $ISHHistory = Join-Path $executingScriptDirectory "ISHGetHistory.ps1"
 $ISHTranlationJob = Join-Path $executingScriptDirectory "ISHUITranslationJob.ps1"
+$ISHUndoDeployment = Join-Path $executingScriptDirectory "UndoVanila.ps1"
 
 Invoke-Expression "$ISHUIContentEditorPath" 
 Invoke-Expression $ISHUIQualityAssistantPath
@@ -15,3 +16,4 @@ Invoke-Expression $ISHUIExternalPreview
 Invoke-Expression $GETISHDeployment
 Invoke-Expression $ISHHistory
 Invoke-Expression $ISHTranlationJob
+Invoke-Expression $ISHUndoDeployment

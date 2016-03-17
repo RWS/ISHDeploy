e94963a1bb33520b841234ff9ebbf72b685e1d7a
Denys Havryliuk <dhavryliuk@sdl.com>
2016-03-17 15:03:48 +0200

    Tests for Clear ISH history implemented test, that where missed - for UndoISHDeployment Added

--- Source/InfoShare.Deployment.PowerShell/AutomationTests/TestInitializer.ps1
+++ Source/InfoShare.Deployment.PowerShell/AutomationTests/TestInitializer.ps1
@@ -5,9 +5,11 @@ $executingScriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition 
 $ISHUIContentEditorPath = Join-Path $executingScriptDirectory "ISHUIContentEditor.ps1"
 $ISHUIQualityAssistantPath =  Join-Path $executingScriptDirectory "ISHUIQualityAssistant.ps1"
 $ISHUIExternalPreview = Join-Path $executingScriptDirectory "ISHUIExternalPreview.ps1"
-$GETISHDeployment = Join-Path $executingScriptDirectory "GET-ISHDeployment.ps1"
+$GETISHDeployment = Join-Path $executingScriptDirectory "Get-ISHDeployment.ps1"
 $ISHHistory = Join-Path $executingScriptDirectory "ISHGetHistory.ps1"
 $ISHTranlationJob = Join-Path $executingScriptDirectory "ISHUITranslationJob.ps1"
+$UndoISHDeployment = Join-Path $executingScriptDirectory "UndoISHDeployment.ps1"
+$ClearISHDeploymentHistory = Join-Path $executingScriptDirectory "ClearISHDeploymentHistory.ps1"
 
 Invoke-Expression "$ISHUIContentEditorPath" 
 Invoke-Expression $ISHUIQualityAssistantPath
@@ -15,3 +17,5 @@ Invoke-Expression $ISHUIExternalPreview
 Invoke-Expression $GETISHDeployment
 Invoke-Expression $ISHHistory
 Invoke-Expression $ISHTranlationJob
+Invoke-Expression $UndoISHDeployment
+Invoke-Expression $ClearISHDeploymentHistory
---
+++ Source/InfoShare.Deployment.PowerShell/AutomationTests/UndoISHDeployment.ps1
@@ -0,0 +1,129 @@
+﻿Import-Module InfoShare.Deployment
+. "$PSScriptRoot\Common.ps1"
+
+#region Variables initoialization
+$htmlStyle = Set-Style
+
+$logFile = "C:\Automated_deployment\Test8.htm"
+
+
+$WarningPreference = “Continue"
+$VerbosePreference = "SilentlyCOntinue"
+$global:logArray = @()
+
+$deploy = Get-ISHDeployment -Name "InfoShareSQL2014"
+
+
+function readTargetXML(){
+#Content Editor XML
+$CExmlPath = $deploy.WebPath+"\Web" + $deploy.OriginalParameters.projectsuffix   + "\Author\ASP\XSL"
+[xml]$XmlFolderButtonbar = Get-Content "$CExmlPath\FolderButtonbar.xml" -ErrorAction SilentlyContinue
+[xml]$XmlInboxButtonBar = Get-Content "$CExmlPath\InboxButtonBar.xml" -ErrorAction SilentlyContinue
+[xml]$XmlLanguageDocumentButtonbar = Get-Content "$CExmlPath\LanguageDocumentButtonbar.xml" -ErrorAction SilentlyContinue
+$global:textFolderButtonbar = $XmlFolderButtonbar.BUTTONBAR.BUTTON.INPUT | ? {$_.NAME -eq "CheckOutWithXopus"}
+$global:textInboxButtonBar = $XmlInboxButtonBar.BUTTONBAR.BUTTON.INPUT | ? {$_.NAME -eq "CheckOutWithXopus"}
+$global:textLanguageDocumentButtonbar = $XmlLanguageDocumentButtonbar.BUTTONBAR.BUTTON.INPUT | ? {$_.NAME -eq "CheckOutWithXopus"}
+
+#External preview XML
+$EPxmlPath = $deploy.WebPath+"\Web" + $deploy.OriginalParameters.projectsuffix   + "\Author\ASP"
+[xml]$XmlWebConfig = Get-Content "$EPxmlPath\Web.config" #-ErrorAction SilentlyContinue
+
+$global:textConfig = $XmlWebConfig.configuration.'trisoft.infoshare.web.externalpreviewmodule'.identity | ? {$_.externalId -eq "ServiceUser"}
+$global:configSection = $XmlWebConfig.configuration.configSections.section | ? {$_.name -eq "trisoft.infoshare.web.externalpreviewmodule"}
+$global:module = $XmlWebConfig.configuration.'system.webServer'.modules.add  | ? {$_.name -eq "TrisoftExternalPreviewModule"}
+$global:configCustomID = $XmlWebConfig.configuration.'trisoft.infoshare.web.externalpreviewmodule'.identity | ? {$_.externalId -eq $customID}
+
+#Quality Assistant XML
+$QAxmlPath =$deploy.WebPath+"\Web" + $deploy.OriginalParameters.projectsuffix  + '\Author\ASP\Editors\Xopus\config'
+[xml]$XmlConfig = Get-Content "$QAxmlPath\config.xml" -ErrorAction SilentlyContinue
+[xml]$XmlBlueLionConfig = Get-Content "$QAxmlPath\bluelion-config.xml" -ErrorAction SilentlyContinue
+
+$global:textConfig = $XmlConfig.config.javascript | ? {$_.src -eq "../BlueLion-Plugin/Bootstrap/bootstrap.js"}
+$global:textBlueLionConfig = $XmlBlueLionConfig.SelectNodes("*/*[local-name()='import'][@src='../BlueLion-Plugin/create-toolbar.xml']")
+
+}
+
+function fileExist(){
+$exist =     
+    (Test-Path ("C:\ProgramData\InfoShare.Deployment\v" + $deploy.SoftwareVersion +"\"+ $deploy.Name  + "\Backup\Web\Author\ASP\Editors\Xopus\config\config.xml")) -and
+    (Test-Path ("C:\ProgramData\InfoShare.Deployment\v" + $deploy.SoftwareVersion +"\"+ $deploy.Name  + "\Backup\Web\Author\ASP\Editors\Xopus\config\bluelion-config.xml")) -and
+    (Test-Path ("C:\ProgramData\InfoShare.Deployment\v" + $deploy.SoftwareVersion +"\"+ $deploy.Name  + "\Backup\Web\Author\ASP\XSL\FolderButtonbar.xml")) -and
+    (Test-Path ("C:\ProgramData\InfoShare.Deployment\v" + $deploy.SoftwareVersion +"\"+ $deploy.Name  + "\Backup\Web\Author\ASP\XSL\InboxButtonBar.xml")) -and
+    (Test-Path ("C:\ProgramData\InfoShare.Deployment\v" + $deploy.SoftwareVersion +"\"+ $deploy.Name  + "\Backup\Web\Author\ASP\XSL\LanguageDocumentButtonbar.xml")) -and
+    (Test-Path ("C:\ProgramData\InfoShare.Deployment\v" + $deploy.SoftwareVersion +"\"+ $deploy.Name  + "\Backup\Web\Author\ASP\Web.config"))
+
+ return $exist   
+}
+
+ Remove-Item ("C:\ProgramData\InfoShare.Deployment\ISH"+$deploy.OriginalParameters.projectsuffix ) -Recurse -Force -ErrorAction SilentlyContinue
+
+function UndoISHDeployment_test(){
+
+    Enable-ISHExternalPreview -ISHDeployment $deploy 
+    Enable-ISHUIContentEditor -ISHDeployment $deploy
+    Enable-ISHUIQualityAssistant -ISHDeployment $deploy
+    #Action
+    readTargetXML
+
+    $CECheck = $textFolderButtonbar -and $textInboxButtonBar -and $textLanguageDocumentButtonbar
+    $QACheck = $textConfig -and $textBlueLionConfig.Count -eq 1
+    $EPCheck = $textConfig -and $configSection -and $module
+
+    $backupFilesExists = fileExist
+
+    $initialCheckResult = $CECheck -and $QACheck -and $EPCheck -and $backupFilesExists
+
+    
+    Undo-ISHDeployment -ISHDeployment $deploy
+
+    readTargetXML
+
+    $CECheck = !$textFolderButtonbar -and !$textInboxButtonBar -and !$textLanguageDocumentButtonbar
+    $QACheck = !$textConfig -and $textBlueLionConfig.Count -eq 0
+    $EPCheck = !$textConfig -and !$configSection -and !$module
+
+    $backupFilesExists = fileExist
+
+    $secondaryCheckResult = !$CECheck -and !$QACheck -and !$EPCheck -and !$backupFilesExists
+
+    $checkResult = $initialCheckResult -and $secondaryCheckResult
+
+    # Assert
+    Assert_IsTrue $checkResult $MyInvocation.MyCommand.Name "1"
+ }
+
+function VanilaBackupWhenCommandletNotSuccesful_test{
+ Remove-Item ("C:\ProgramData\InfoShare.Deployment\v" + $deploy.SoftwareVersion +"\"+ $deploy.Name ) -Recurse -Force -ErrorAction SilentlyContinue
+
+ $targetFilePath = $deploy.WebPath+"\Web" + $deploy.OriginalParameters.projectsuffix  + '\Author\ASP\Editors\Xopus\config\'
+
+ Rename-Item $targetFilePath"bluelion-config.xml" "_bluelion-config.xml"
+
+ try{
+  Enable-ISHUIQualityAssistant -ISHDeployment $deploy  -ErrorAction Stop
+  }
+ catch{
+ 
+ }
+
+    $checkResult = Test-Path ("C:\ProgramData\InfoShare.Deployment\v" + $deploy.SoftwareVersion +"\"+ $deploy.Name  + "\Backup\Web\Author\ASP\Editors\Xopus\config\config.xml")
+     Rename-Item $targetFilePath"_bluelion-config.xml" "bluelion-config.xml"
+ # Assert
+    Assert_IsTrue $checkResult $MyInvocation.MyCommand.Name "2"
+ }
+
+
+
+
+# Remove-Item ("C:\ProgramData\InfoShare.Deployment\v" + $deploy.SoftwareVersion + "\"+ $deploy.Name ) -Recurse -Force -ErrorAction SilentlyContinue
+
+UndoISHDeployment_test
+VanilaBackupWhenCommandletNotSuccesful_test
+
+$global:logArray | ConvertTo-HTML -Head $htmlStyle | Out-File $logFile
+
+
+Edit-LogHtml -targetHTML $logFile
+
+
+#Invoke-Expression $logFile

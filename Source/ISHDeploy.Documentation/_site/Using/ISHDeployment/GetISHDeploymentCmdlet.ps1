711c19c05309c2a2573cac7d5176fae02bbff571
Orlov Oleksandr <oorlov@sdl.com>
2016-03-24 17:31:55 +0200

    Removing unnecessary folders and files.

--- Source/ISHDeploy.Documentation/_site/Using/ISHDeployment/GetISHDeploymentCmdlet.ps1
+++
@@ -1,9 +0,0 @@
-CLS
-Import-Module ISHDeploy
-
-$DebugPreference = "Continue"
-$VerbosePreference = "Continue"
-$WarningPreference = "Continue"
-
-Get-ISHDeployment -Deployment "" | Set-ISHDeployment
-

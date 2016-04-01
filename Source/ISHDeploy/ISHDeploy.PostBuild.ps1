8ba16bc2da0067a0fa43c46d47aff6ff2463b2f8
Orlov Oleksandr <oorlov@sdl.com>
2016-04-01 15:58:16 +0300

    Fixed quotes and PS postbuild.ps

--- Source/ISHDeploy/ISHDeploy.PostBuild.ps1
+++ Source/ISHDeploy/ISHDeploy.PostBuild.ps1
@@ -1,11 +1,5 @@
 ﻿param([String]$outputpath, [String]$modulename)
 
-<<<<<<< HEAD
-=======
-# Remove quotes
-$outputpath = $outputpath.trim("'");
-
->>>>>>> Added file Build.props that contains all the variables that might be different for all branches.
 # Get PS module path
 [array]$modulepath = $env:PSModulePath.Split(";")
 if ($modulepath) {
@@ -19,9 +13,5 @@ if ((Test-Path -path "$modulepath\$modulename") -ne $True)
 }
 
 # Copy files to WindowsPowerShell directory
-<<<<<<< HEAD
-#Get-ChildItem -Path $outputpath -Filter ("$modulename*") | Copy-Item -Destination "$modulepath\$modulename" -Force
-=======
 Get-ChildItem -Path $outputpath -Filter ("$modulename*") | Copy-Item -Destination "$modulepath\$modulename" -Force
 
->>>>>>> Added file Build.props that contains all the variables that might be different for all branches.

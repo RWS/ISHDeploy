4121ad0146f56e5693814be09c7dfcbdcf81ab27
Orlov Oleksandr <oorlov@sdl.com>
2016-04-20 17:22:52 +0300

    Added modifications to CheckDeployment version method

--- Source/ISHDeploy/Validators/ValidateDeploymentVersion.cs
+++ Source/ISHDeploy/Validators/ValidateDeploymentVersion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Management.Automation;
+using System.Text.RegularExpressions;
 using ISHDeploy.Models;
 
 namespace ISHDeploy.Validators
@@ -26,51 +27,50 @@ namespace ISHDeploy.Validators
         /// </exception>
         protected override void Validate(object arguments, EngineIntrinsics engineIntrinsics)
         {
-            ISHDeployment deployment;
-            if ((deployment = arguments as ISHDeployment) == null)
+            ISHDeployment deployment = arguments as ISHDeployment;
+            if (deployment == null)
             {
                 throw new ValidationMetadataException($"Validation {nameof(ValidateDeploymentVersion)} should be only applied to parameter with type {nameof(ISHDeployment)}");
             }
 
             string warningMessage;
-
             if (!CheckDeploymentVersion(deployment.SoftwareVersion, out warningMessage))
             {
                 throw new ValidationMetadataException(warningMessage);
             }
         }
 
-        /// <summary>
-        /// Checks if deployment version corresponds to module version.
-        /// </summary>
-        /// <param name="deploymentVersion">The deployment version.</param>
-        /// <param name="warningMessage">The warning message in case versions are different.</param>
-        /// <returns>True if versions correspond; otherwise False.</returns>
-        public static bool CheckDeploymentVersion(Version deploymentVersion, out string warningMessage)
-        {
-            var moduleName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
-            var moduleVersion = new Version(moduleName.Substring(moduleName.IndexOf('.') + 1));
-            
-            var cmVersion = new Version($"{deploymentVersion.Major}.{deploymentVersion.Minor}.{deploymentVersion.Revision}"); // don't count about Build version.
+		/// <summary>
+		/// Checks if deployment version corresponds to module version.
+		/// </summary>
+		/// <param name="deploymentVersion">The deployment version.</param>
+		/// <param name="warningMessage">The warning message in case versions are different.</param>
+		/// <returns>True if versions correspond; otherwise False.</returns>
+		public static bool CheckDeploymentVersion(Version deploymentVersion, out string warningMessage)
+		{
+			warningMessage = null;
+			var moduleName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+			var cmVersion = new Version(deploymentVersion.Major, deploymentVersion.Minor, deploymentVersion.Revision); // don't count about Build version.
 
-            if (cmVersion != moduleVersion)
-            {
-                warningMessage = $"Module version {moduleVersion} does not correspond to deployment version {deploymentVersion}.";
+			Regex regex = new Regex("^\\w+\\.(?<MajorVersion>\\d+)\\.(?<MinorVersion>\\d+)\\.(?<Revision>\\d+)$");
+			Version moduleVersion;
+			if (regex.IsMatch(moduleName) &&
+	            Version.TryParse(regex.Replace(moduleName, "${MajorVersion}.${MinorVersion}.${Revision}"),
+		            out moduleVersion))
+			{
+				int cmp = cmVersion.CompareTo(moduleVersion);
+				if (cmp != 0)
+				{
+					warningMessage =
+						$"Module version `{moduleVersion}` does not correspond to deployment version `{deploymentVersion}`.\r\nPlease install {(cmp > 0 ? $"module {moduleName}." : "newer Content Manager version.")}";
+				}
+			}
+	        else
+	        {
+				warningMessage = $"Module name `{moduleName}` has different definition than expected.";
+			}
 
-                if (cmVersion >= ModuleInitVersion)
-                {
-                    warningMessage += $"\r\nPlease install module {moduleName}.";
-                }
-                else
-                {
-                    warningMessage += "\r\nPlease install newer Content Manager version.";
-                }
-
-                return false;
-            }
-
-            warningMessage = string.Empty;
-            return true;
-        }
+			return String.IsNullOrEmpty(warningMessage);
+		}
     }
 }

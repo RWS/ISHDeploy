d9a407aa41b53d5eb3384ec2eb810c3d5fc2ae33
Volodymyr Maksymenko <vmaksymenko@sdl.com>
2016-06-13 21:31:26 +0300

    Add Copyright (c) 2014 All Rights Reserved by the SDL Group.

--- Source/ISHDeploy.Tests/Data/Actions/File/SavePackageActionTest.cs
+++ Source/ISHDeploy.Tests/Data/Actions/File/SavePackageActionTest.cs
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-﻿using ISHDeploy.Data.Actions.Directory;
+using ISHDeploy.Data.Actions.Directory;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
 

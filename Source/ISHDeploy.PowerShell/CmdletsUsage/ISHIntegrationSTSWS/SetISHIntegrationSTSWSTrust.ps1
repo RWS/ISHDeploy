7f19b26830b4dbbaaa6c5b2183fbc64ed566c7aa
ashatrov <ashatrov@sdl.com>
2016-06-15 17:13:20 +0300

    Moved CmdletsUsage directory to ishdeploy-build project.

--- Source/ISHDeploy.PowerShell/CmdletsUsage/ISHIntegrationSTSWS/SetISHIntegrationSTSWSTrust.ps1
+++
@@ -1,3 +0,0 @@
-﻿#$deployment = Get-ISHDeployment
-
-Set-ISHIntegrationSTSWSTrust -ISHDeployment $deployment[0] -Endpoint "https://mecdev12qa02.global.sdl.corp/ISHSTSORA12/issue/wstrust/mixed/username" -MexEndpoint "https://mecdev12qa02.global.sdl.corp/ISHSTSORA12/issue/wstrust/mex" -BindingType "UserNameMixed" -ActorUsername "admin" -ActorPassword "admin" -Verbose -Debug

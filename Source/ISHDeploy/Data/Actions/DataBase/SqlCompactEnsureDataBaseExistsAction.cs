bfd65c46da86bdf69d55436440ffe2929a71ba20
Orlov Oleksandr <oorlov@sdl.com>
2016-07-07 17:41:43 +0300

    Implemented

--- Source/ISHDeploy/Data/Actions/DataBase/SqlCompactEnsureDataBaseExistsAction.cs
+++ Source/ISHDeploy/Data/Actions/DataBase/SqlCompactEnsureDataBaseExistsAction.cs
@@ -1,18 +1,18 @@
 ﻿/**
-* Copyright (c) 2014 All Rights Reserved by the SDL Group.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (c) 2014 All Rights Reserved by the SDL Group.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 using System;
 using System.Net;
 using ISHDeploy.Data.Exceptions;
@@ -50,8 +50,8 @@ namespace ISHDeploy.Data.Actions.DataBase
         /// <param name="logger">The logger.</param>
         /// <param name="dbFilePath">The database file path.</param>
         /// <param name="baseUrl">The base URL.</param>
-        public SqlCompactEnsureDataBaseExistsAction(ILogger logger, string dbFilePath, string baseUrl)
-            : base(logger)
+        public SqlCompactEnsureDataBaseExistsAction(ILogger logger, string dbFilePath, string baseUrl) 
+			: base(logger)
         {
             _dbFilePath = dbFilePath;
             _baseUrl = baseUrl;
@@ -79,7 +79,7 @@ namespace ISHDeploy.Data.Actions.DataBase
             request.Method = "GET";
             request.Timeout = 10000;
             request.KeepAlive = false;
-
+                
             if (!_fileManager.FileExists(_dbFilePath))
             {
                 try

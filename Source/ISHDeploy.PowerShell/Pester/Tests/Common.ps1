bf3fc66d023147d39825a6359cf0f60585a5ca06
Denys Havryliuk <dhavryliuk@sdl.com>
2016-07-26 11:43:45 +0300

    removed timeout

--- Source/ISHDeploy.PowerShell/Pester/Tests/Common.ps1
+++ Source/ISHDeploy.PowerShell/Pester/Tests/Common.ps1
@@ -77,7 +77,6 @@ $scriptBlockWebRequest = {
     
     $request = [System.Net.WebRequest]::Create($url)
     $request.Method = "GET";
-    $request.Timeout = 20000;
     $request.KeepAlive = $false;
     try {
         [System.Net.HttpWebResponse]$response = $request.GetResponse()

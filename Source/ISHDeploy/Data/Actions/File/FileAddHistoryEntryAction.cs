3ada800a42d45392640f7fd38fa8864ec676dbf1
Volodymyr Maksymenko <vmaksymenko@sdl.com>
2016-07-28 10:04:45 +0300

    Fix according to review remark

--- Source/ISHDeploy/Data/Actions/File/FileAddHistoryEntryAction.cs
+++ Source/ISHDeploy/Data/Actions/File/FileAddHistoryEntryAction.cs
@@ -52,7 +52,7 @@ namespace ISHDeploy.Data.Actions.File
         private readonly IFileManager _fileManager;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="FileCopyToDirectoryAction"/> class.
+        /// Initializes a new instance of the <see cref="FileAddHistoryEntryAction"/> class.
         /// </summary>
         /// <param name="logger">The logger.</param>
         /// <param name="path">The file path.</param>

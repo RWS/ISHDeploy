d57d608f96cf2c08cc9f801f71cd13cbe2327313
Alex Sarafian <asarafian@sdl.com>
2016-08-01 16:37:30 +0200

    New site for alpha.12.0.0-0.2

---
+++ 12.0.0/Articles/FeatureToggle/02.ModifyCustomTab.ps1
@@ -0,0 +1,7 @@
+﻿$hash=@{
+    Label="Custom Event"
+    Description="Show all custom events"
+    EventTypesFilter=@("CUSTOM1","CUSTOM2")
+}
+Set-ISHUIEventMonitorTab -ISHDeployment $deploymentName @hash
+Move-ISHUIEventMonitorTab -ISHDeployment $deploymentName -Label $hash["Label"] -First

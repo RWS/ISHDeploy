af666dddd024830b7d0b05c44ce087e92e7ca93f
vmaksymenko <vmaksymenko@sdl.com>
2017-01-19 17:19:16 +0200

    Set service recovery options

--- Source/ISHDeploy/Data/Resources/Install-WindowsService.ps1
+++ Source/ISHDeploy/Data/Resources/Install-WindowsService.ps1
@@ -2,3 +2,5 @@
 $secureString = "$password" | ConvertTo-SecureString -asPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential ("$username", $secureString)
 New-Service -Name "$name" -DisplayName "$displayName" -Description "$description" -BinaryPathName "$pathToExecutable" -StartupType Manual -Credential $credential
+
+sc.exe failure "$name" reset= 86400 reset= 0 actions= "restart/60000"

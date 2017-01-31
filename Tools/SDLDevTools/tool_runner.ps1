a3b5e67b837a9a562ae9ba96719e0821022fbbcf
vmaksymenko <vmaksymenko@sdl.com>
2017-01-31 14:57:09 +0200

    Changes according to remarks

--- Tools/SDLDevTools/tool_runner.ps1
+++ Tools/SDLDevTools/tool_runner.ps1
@@ -1,10 +1,10 @@
 ﻿param(
-[string]$PathToModule,
 [string]$PathToFolderThatMustBeTested,
 [string]$ExcludeFolders,
 [string]$ExcludeExtensions
 )
 
+$PathToModule = Join-Path $PSScriptRoot "Modules\SDLDevTools"
 Import-Module $PathToModule
 
 Test-SDLOpenSourceHeader -FolderPath $PathToFolderThatMustBeTested -ExpandError -PassThru -ExcludeFolder $ExcludeFolders.Split(",") -ExcludeExtension $ExcludeExtensions.Split(",")

3b342eb82486ca72843068e9cd67e5d38800b91c
vmaksymenko <vmaksymenko@sdl.com>
2017-02-06 14:56:54 +0200

    Minor code fixes

--- Source/ISHDeploy/Business/Operations/ISHServiceTranslation/SetISHIntegrationWorldServerOperation.cs
+++ Source/ISHDeploy/Business/Operations/ISHServiceTranslation/SetISHIntegrationWorldServerOperation.cs
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+using System;
 using ISHDeploy.Business.Invokers;
 using ISHDeploy.Common.Interfaces;
 using ISHDeploy.Common.Models;
@@ -33,7 +34,7 @@ namespace ISHDeploy.Business.Operations.ISHServiceTranslation
         private readonly IActionInvoker _invoker;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="SetISHServiceTranslationOrganizerOperation"/> class.
+        /// Initializes a new instance of the <see cref="SetISHIntegrationWorldServerOperation"/> class.
         /// </summary>
         /// <param name="logger">The logger.</param>
         /// <param name="ishDeployment">The instance of the deployment.</param>
@@ -51,7 +52,7 @@ namespace ISHDeploy.Business.Operations.ISHServiceTranslation
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="SetISHServiceTranslationOrganizerOperation"/> class.
+        /// Initializes a new instance of the <see cref="SetISHIntegrationWorldServerOperation"/> class.
         /// </summary>
         /// <param name="logger">The logger.</param>
         /// <param name="ishDeployment">The instance of the deployment.</param>
--- Source/ISHDeploy/Cmdlets/ISHServiceTranslation/SetISHIntegrationWorldServerCmdlet.cs
+++ Source/ISHDeploy/Cmdlets/ISHServiceTranslation/SetISHIntegrationWorldServerCmdlet.cs
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+using System;
 using System.Management.Automation;
 using System.Runtime.InteropServices;
 using ISHDeploy.Business.Operations.ISHServiceTranslation;

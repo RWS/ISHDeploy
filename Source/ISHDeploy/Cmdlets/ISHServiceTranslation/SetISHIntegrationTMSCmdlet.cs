3161928c722997254d76060d96aebcc0ab8ff275
vmaksymenko <vmaksymenko@sdl.com>
2017-02-07 19:25:19 +0200

    First implementation

---
+++ Source/ISHDeploy/Cmdlets/ISHServiceTranslation/SetISHIntegrationTMSCmdlet.cs
@@ -0,0 +1,174 @@
+/*
+ * Copyright (c) 2014 All Rights Reserved by the SDL Group.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Management.Automation;
+using System.Runtime.InteropServices;
+using ISHDeploy.Business.Operations.ISHServiceTranslation;
+using ISHDeploy.Common.Models.TranslationOrganizer;
+
+namespace ISHDeploy.Cmdlets.ISHServiceTranslation
+{
+    /// <summary>
+    /// <para type="synopsis">Sets configuration of TMS.</para>
+    /// <para type="description">The Set-ISHIntegrationTMS cmdlet sets configuration of TMS.</para>
+    /// <para type="link">New-ISHIntegrationTMSMapping</para>
+    /// <para type="link">New-ISHTMSTemplate</para>
+    /// <para type="link">New-ISHFieldMetadata</para>
+    /// </summary>
+    /// <example>
+    /// <code>PS C:\>Set-ISHIntegrationTMS -ISHDeployment $deployment -Name "ws1" -Uri "https:\\ws1.sd.com" -Credential $credential -MaximumJobSize 5242880 -RetriesOnTimeout 3 -Mapping $mapping -Templates $templates</code>
+    /// <para>This command enables the translation organizer windows service.
+    /// Parameter $deployment is a deployment name or an instance of the Content Manager deployment retrieved from Get-ISHDeployment cmdlet.
+    /// Parameter $credential is a set of security credentials, such as a user name and a password.
+    /// Parameter $mapping is a object with pair of properties, where ISHLanguage is InfoShare language identifier retrieved from New-ISHIntegrationTMSMapping cmdlet.</para>
+    /// </example>
+    [Cmdlet(VerbsCommon.Set, "ISHIntegrationTMS")]
+    public sealed class SetISHIntegrationTMSCmdlet : BaseHistoryEntryCmdlet
+    {
+        /// <summary>
+        /// <para type="description">The name (alias) of TMS.</para>
+        /// </summary>
+        [Parameter(Mandatory = true, HelpMessage = "The name (alias) of TMS")]
+        [ValidateNotNullOrEmpty]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// <para type="description">The Uri to TMS.</para>
+        /// </summary>
+        [Parameter(Mandatory = true, HelpMessage = "The Uri to TMS")]
+        [ValidateNotNullOrEmpty]
+        public string Uri { get; set; }
+
+        /// <summary>
+        /// <para type="description">The credential to get access to TMS.</para>
+        /// </summary>
+        [Parameter(Mandatory = true, HelpMessage = "The credential to get access to TMS")]
+        [ValidateNotNullOrEmpty]
+        public PSCredential Credential { get; set; }
+
+        /// <summary>
+        /// <para type="description">The max value of total size in bytes of uncompressed external job.</para>
+        /// </summary>
+        [Parameter(Mandatory = true, HelpMessage = "The max value of total size in bytes of uncompressed external job")]
+        [ValidateNotNullOrEmpty]
+        public int MaximumJobSize { get; set; }
+
+        /// <summary>
+        /// <para type="description">The number of retries on timeout.</para>
+        /// </summary>
+        [Parameter(Mandatory = true, HelpMessage = "The number of retries on timeout.")]
+        [ValidateNotNullOrEmpty]
+        [ValidateRange(1, 30)]
+        public int RetriesOnTimeout { get; set; }
+
+        /// <summary>
+        /// <para type="description">The mapping between ISHLanguage and TmsLanguage.</para>
+        /// </summary>
+        [Parameter(Mandatory = true, HelpMessage = "The mapping between trisoftLanguage and TmsLanguage.")]
+        [ValidateNotNullOrEmpty]
+        public ISHLanguageToTmsLanguageMapping[] Mappings { get; set; }
+
+        /// <summary>
+        /// <para type="description">The TMS templates.</para>
+        /// </summary>
+        [Parameter(Mandatory = true, HelpMessage = "The mapping between trisoftLanguage and TmsLanguage.")]
+        [ValidateNotNullOrEmpty]
+        public TmsTemplate[] Templates { get; set; }
+
+        /// <summary>
+        /// <para type="description">The destination port number.</para>
+        /// </summary>
+        [Parameter(Mandatory = false, HelpMessage = "The destination port number.")]
+        public int DestinationPortNumber { get; set; }
+
+        /// <summary>
+        /// <para type="description">The location of Isapi filter.</para>
+        /// </summary>
+        [Parameter(Mandatory = false, HelpMessage = "The location of Isapi filter.")]
+        public string IsapiFilterLocation { get; set; }
+
+        /// <summary>
+        /// <para type="description">Use compression</para>
+        /// </summary>
+        [Parameter(Mandatory = false, HelpMessage = "Use compression")]
+        public bool UseCompression { get; set; }
+
+        /// <summary>
+        /// <para type="description">Use SSL.</para>
+        /// </summary>
+        [Parameter(Mandatory = false, HelpMessage = "Use SSL.")]
+        public bool UseSsl { get; set; }
+
+        /// <summary>
+        /// <para type="description">Use default proxy credentials.</para>
+        /// </summary>
+        [Parameter(Mandatory = false, HelpMessage = "Use default proxy credentials.")]
+        public bool UseDefaultProxyCredentials { get; set; }
+
+        /// <summary>
+        /// <para type="description">The proxy server.</para>
+        /// </summary>
+        [Parameter(Mandatory = false, HelpMessage = "The proxy server.")]
+        public string ProxyServer { get; set; }
+
+        /// <summary>
+        /// <para type="description">The port number of proxy server.</para>
+        /// </summary>
+        [Parameter(Mandatory = false, HelpMessage = "The port number of proxy server.")]
+        public int ProxyPort { get; set; }
+
+        /// <summary>
+        /// <para type="description">The requested metadata.</para>
+        /// </summary>
+        [Parameter(Mandatory = false, HelpMessage = "The requested metadata.")]
+        public ISHFieldMetadata RequestedMetadata { get; set; }
+
+        /// <summary>
+        /// <para type="description">The grouping metadata.</para>
+        /// </summary>
+        [Parameter(Mandatory = false, HelpMessage = "The grouping metadata.")]
+        public ISHFieldMetadata GroupingMetadata { get; set; }
+
+        /// <summary>
+        /// Executes cmdlet
+        /// </summary>
+        public override void ExecuteCmdlet()
+        {
+            var tmsConfiguration = new TmsConfigurationSection(
+                Name,
+                Uri,
+                Credential.UserName,
+                Marshal.PtrToStringUni(Marshal.SecureStringToGlobalAllocUnicode(Credential.Password)),
+                MaximumJobSize,
+                RetriesOnTimeout,
+                Mappings,
+                Templates,
+                DestinationPortNumber,
+                IsapiFilterLocation,
+                UseCompression,
+                UseSsl,
+                UseDefaultProxyCredentials,
+                ProxyServer,
+                ProxyPort,
+                RequestedMetadata,
+                GroupingMetadata);
+
+            var operation = new SetISHIntegrationTmsOperation(Logger, ISHDeployment, tmsConfiguration);
+
+            operation.Run();
+        }
+    }
+}
---
+++ Source/ISHDeploy/Common/Models/TranslationOrganizer/TmsConfigurationSection.cs
@@ -0,0 +1,189 @@
+﻿/*
+ * Copyright (c) 2014 All Rights Reserved by the SDL Group.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Xml.Serialization;
+
+namespace ISHDeploy.Common.Models.TranslationOrganizer
+{
+    /// <summary>
+    /// <para type="description">Represents configuration of an instance of TMS.</para>
+    /// </summary>
+    [System.Serializable()]
+    [System.ComponentModel.DesignerCategory("code")]
+    [XmlType(AnonymousType = true)]
+    [XmlRoot("add", Namespace = "", IsNullable = false)]
+    public class TmsConfigurationSection : BaseXMLElement
+    {
+        /// <summary>
+        /// The alias of WorldServer 
+        /// </summary>
+        [XmlAttribute("alias")]
+        public string Alias { get; set; }
+
+        /// <summary>
+        /// The Uri to WorldServer 
+        /// </summary>
+        [XmlAttribute("uri")]
+        public string Uri { get; set; }
+
+        /// <summary>
+        /// The userName to get access to WorldServer
+        /// </summary>
+        [XmlAttribute("userName")]
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// The password to get access to WorldServer
+        /// </summary>
+        [XmlAttribute("password")]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// The max value of total size in bytes of uncompressed external job
+        /// </summary>
+        [XmlAttribute("externalJobMaxTotalUncompressedSizeBytes")]
+        public int ExternalJobMaxTotalUncompressedSizeBytes { get; set; }
+
+        /// <summary>
+        /// The number of retries on timeout
+        /// </summary>
+        [XmlAttribute("retriesOnTimeout")]
+        public int RetriesOnTimeout { get; set; }
+
+        /// <summary>
+        /// The mapping between trisoftLanguage and worldServerLocaleId
+        /// </summary>
+        [XmlArray("mappings")]
+        [XmlArrayItem("add", IsNullable = false)]
+        public ISHLanguageToTmsLanguageMapping[] Mappings { get; set; }
+
+        /// <summary>
+        /// Tms templates
+        /// </summary>
+        [XmlArray("templates")]
+        [XmlArrayItem("add", IsNullable = false)]
+        public TmsTemplate[] Templates { get; set; }
+
+        ///// <summary>
+        ///// The destination port number
+        ///// </summary>
+        //[XmlAttribute("destinationPortNumber")]
+        //public int? DestinationPortNumber { get; set; }
+
+        ///// <summary>
+        ///// The location of Isapi filter
+        ///// </summary>
+        //[XmlAttribute("isapiFilterLocation")]
+        //public string IsapiFilterLocation { get; set; }
+
+        ///// <summary>
+        ///// Use compression
+        ///// </summary>
+        //[XmlAttribute("useCompression")]
+        //public bool? UseCompression { get; set; }
+
+        ///// <summary>
+        ///// Use SSL
+        ///// </summary>
+        //[XmlAttribute("useSsl")]
+        //public bool? UseSsl { get; set; }
+
+        ///// <summary>
+        ///// Use default proxy credentials
+        ///// </summary>
+        //[XmlAttribute("useDefaultProxyCredentials")]
+        //public bool? UseDefaultProxyCredentials { get; set; }
+
+        ///// <summary>
+        ///// The proxy server
+        ///// </summary>
+        //[XmlAttribute("proxyServer")]
+        //public string ProxyServer { get; set; }
+
+        ///// <summary>
+        ///// The port number of proxy server
+        ///// </summary>
+        //[XmlAttribute("proxyPort")]
+        //public int? ProxyPort { get; set; }
+        
+        ///// <summary>
+        ///// The requested metadata
+        ///// </summary>
+        //[XmlAttribute("requestedMetadata")]
+        //public ISHFieldMetadata RequestedMetadata { get; set; }
+
+        ///// <summary>
+        ///// The grouping metadata
+        ///// </summary>
+        //[XmlAttribute("groupingMetadata")]
+        //public ISHFieldMetadata GroupingMetadata { get; set; }
+
+        /// <summary>
+        /// Prevents a default instance of the <see cref="TmsConfigurationSection"/> class from being created.
+        /// </summary>
+        private TmsConfigurationSection()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TmsConfigurationSection"/> class.
+        /// </summary>
+        /// <param name="alias">The alias of WorldServer</param>
+        /// <param name="uri">The Uri to WorldServer </param>
+        /// <param name="userName">The userName to get access to WorldServer</param>
+        /// <param name="password">The password to get access to WorldServer</param>
+        /// <param name="externalJobMaxTotalUncompressedSizeBytes">The max value of total size in bytes of uncompressed external job</param>
+        /// <param name="retriesOnTimeout">The number of retries on timeout</param>
+        /// <param name="mappings">The mapping between trisoftLanguage and worldServerLocaleId</param>
+        /// <param name="templates">Tms templates</param>
+        /// <param name="destinationPortNumber">The destination port number</param>
+        /// <param name="isapiFilterLocation">The location of Isapi filter</param>
+        /// <param name="useCompression">Use compression</param>
+        /// <param name="useSsl">Use SSL</param>
+        /// <param name="useDefaultProxyCredentials">Use default proxy credentials</param>
+        /// <param name="proxyServer">The proxy server</param>
+        /// <param name="proxyPort">The port number of proxy server</param>
+        /// <param name="requestedMetadata">The port number of proxy server</param>
+        /// <param name="groupingMetadata">The port number of proxy server</param>
+        public TmsConfigurationSection(string alias, string uri, string userName, string password, int externalJobMaxTotalUncompressedSizeBytes, int retriesOnTimeout, ISHLanguageToTmsLanguageMapping[] mappings, TmsTemplate[] templates, int? destinationPortNumber = null, string isapiFilterLocation = null, bool? useCompression = null, bool? useSsl = null, bool? useDefaultProxyCredentials = null, string proxyServer = null, int? proxyPort = null, ISHFieldMetadata requestedMetadata = null, ISHFieldMetadata groupingMetadata = null)
+        {
+            Alias = alias;
+            Uri = uri;
+            UserName = userName;
+            Password = password;
+            ExternalJobMaxTotalUncompressedSizeBytes = externalJobMaxTotalUncompressedSizeBytes;
+            RetriesOnTimeout = retriesOnTimeout;
+            Mappings = mappings;
+            Templates = templates;
+            //DestinationPortNumber = destinationPortNumber;
+            //IsapiFilterLocation = isapiFilterLocation;
+            //UseCompression = useCompression;
+            //UseSsl = useSsl;
+            //UseDefaultProxyCredentials = useDefaultProxyCredentials;
+            //ProxyServer = proxyServer;
+            //ProxyPort = proxyPort;
+            //RequestedMetadata = requestedMetadata;
+            //GroupingMetadata = groupingMetadata;
+
+            RelativeFilePath = @"TranslationOrganizer\Bin\TranslationOrganizer.exe.config";
+            XPathToParentElement = "configuration/trisoft.infoShare.translationOrganizer/tms/instances";
+            NameOfItem = "add";
+
+            XPathFormat = "configuration/trisoft.infoShare.translationOrganizer/tms/instances/add[@alias='{0}']";
+            XPath = string.Format(XPathFormat, alias);
+        }
+    }
+}

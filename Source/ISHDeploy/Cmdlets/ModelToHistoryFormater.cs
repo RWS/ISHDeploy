f2202e87278714134fea2a356089ab5f1183d56e
vmaksymenko <vmaksymenko@sdl.com>
2017-02-14 12:19:51 +0200

    Add history support

--- Source/ISHDeploy/Cmdlets/ModelToHistoryFormater.cs
+++ Source/ISHDeploy/Cmdlets/ModelToHistoryFormater.cs
@@ -41,6 +41,13 @@ namespace ISHDeploy.Cmdlets
                     $"(New-ISHIntegrationWorldServerMapping -ISHLanguage {model.ISHLanguage} -WSLocaleID {model.WSLocaleID})";
             }
 
+            if (obj is ISHLanguageToTmsLanguageMapping)
+            {
+                var model = obj as ISHLanguageToTmsLanguageMapping;
+                return
+                    $"(New-ISHIntegrationTMSMapping -TmsLanguage {model.TmsLanguage} -ISHLanguage {model.ISHLanguage})";
+            }
+
             return needQuotesIfObjectIsNotRecognized ? $"\"{obj}\"" : $"{obj}";
         }
     }

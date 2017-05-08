39e7f56e9a05de13c5b4a43ba5db3186d8450c70
vmaksymenko <vmaksymenko@sdl.com>
2017-05-08 17:37:43 +0300

    Fix logic for COMPlus

--- Source/ISHDeploy/Data/Resources/Set-COMPlusComponentCredentials.ps1
+++ Source/ISHDeploy/Data/Resources/Set-COMPlusComponentCredentials.ps1
@@ -3,8 +3,8 @@ $comAdmin = New-Object -com ("COMAdmin.COMAdminCatalog.1")
 $catalog = New-Object -com COMAdmin.COMAdminCatalog 
 $applications = $catalog.getcollection("Applications") 
 $applications.populate()
-$trisoftInfoShareAuthorApplication=$applications|Where-Object -Property Name -EQ $name
+$trisoftInfoShareAuthorApplication=$applications|Where-Object -Property Name -EQ "$name"
 
-$trisoftInfoShareAuthorApplication.Value("Identity") = $username
-$trisoftInfoShareAuthorApplication.Value("Password") = $password
+$trisoftInfoShareAuthorApplication.Value("Identity") = "$username"
+$trisoftInfoShareAuthorApplication.Value("Password") = "$password"
 $applications.SaveChanges();

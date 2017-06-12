1e58d6d4a45968dc2b9d1ce916b6fe4e2b8f034d
vmaksymenko <vmaksymenko@sdl.com>
2017-06-12 13:14:52 +0300

    TS-12412 - Add our own waiting of windows service status method

--- Source/ISHDeploy.PowerShell/Pester/Tests/ISHServiceCrawler.Tests.ps1
+++ Source/ISHDeploy.PowerShell/Pester/Tests/ISHServiceCrawler.Tests.ps1
@@ -51,90 +51,90 @@ $scriptBlockGetISHServiceCrawler = {
 }
 
 Describe "Testing ISHServiceCrawler"{
-  #  BeforeEach {
-		#UndoDeploymentBackToVanila $testingDeploymentName $true
-  #  }
+    BeforeEach {
+		UndoDeploymentBackToVanila $testingDeploymentName $true
+    }
     
-  #  It "Enable-ISHServiceCrawler Disabled Crawler Service"{
-		##Arrange
-  #      # Make sure, that Crawler Service is disabled, otherwise - make it disabled 
-  #      $precondition = (Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status
-  #      if ($precondition -eq "Running"){Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockDisableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName }
-  #      # Now Crawler Service should be fo sure disabled. Otherwise test fails
-  #      RetryCommand -numberOfRetries 10 -command {(Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status} -expectedResult "Stopped"| Should be "Stopped"
-		##Act
-  #      # Try enabling Crawler Service
-  #      Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockEnableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName
-  #      RetryCommand -numberOfRetries 10 -command {(Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status} -expectedResult "Running"| Should be "Running"
+    It "Enable-ISHServiceCrawler Disabled Crawler Service"{
+		#Arrange
+        # Make sure, that Crawler Service is disabled, otherwise - make it disabled 
+        $precondition = (Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status
+        if ($precondition -eq "Running"){Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockDisableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName }
+        # Now Crawler Service should be fo sure disabled. Otherwise test fails
+        RetryCommand -numberOfRetries 10 -command {(Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status} -expectedResult "Stopped"| Should be "Stopped"
+		#Act
+        # Try enabling Crawler Service
+        Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockEnableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName
+        RetryCommand -numberOfRetries 10 -command {(Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status} -expectedResult "Running"| Should be "Running"
 
-  #  }
+    }
     
-  #  It "Disable-ISHServiceCrawler enabled Crawler Service" {
-		##Arrange
-  #      # Make sure, that Crawler Service is enabled, otherwise - make it enabled
-  #      $precondition = (Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status
-  #      if ($precondition -eq "Stopped"){Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockEnableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName }
-  #      # Now Crawler Service should be fo sure enabled. Otherwise test fails
-  #      RetryCommand -numberOfRetries 10 -command {(Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status} -expectedResult "Running"| Should be "Running"
-		##Act
-  #      # Try enabling Crawler Service
-  #      Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockDisableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName
-  #      RetryCommand -numberOfRetries 10 -command {(Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status} -expectedResult "Stopped"| Should be "Stopped"
-  #  }
+    It "Disable-ISHServiceCrawler enabled Crawler Service" {
+		#Arrange
+        # Make sure, that Crawler Service is enabled, otherwise - make it enabled
+        $precondition = (Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status
+        if ($precondition -eq "Stopped"){Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockEnableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName }
+        # Now Crawler Service should be fo sure enabled. Otherwise test fails
+        RetryCommand -numberOfRetries 10 -command {(Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status} -expectedResult "Running"| Should be "Running"
+		#Act
+        # Try enabling Crawler Service
+        Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockDisableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName
+        RetryCommand -numberOfRetries 10 -command {(Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status} -expectedResult "Stopped"| Should be "Stopped"
+    }
     
     
-  #   It "Enable-ISHServiceCrawler enabled Crawler Service"{
-		##Arrange
-  #      # Make sure, that Crawler Service is disabled, otherwise - make it disabled 
-  #      $precondition = (Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status
-  #      if ($precondition -eq "Running"){Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockDisableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName }
+     It "Enable-ISHServiceCrawler enabled Crawler Service"{
+		#Arrange
+        # Make sure, that Crawler Service is disabled, otherwise - make it disabled 
+        $precondition = (Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status
+        if ($precondition -eq "Running"){Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockDisableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName }
 
-  #      # Now Crawler Service should be fo sure disabled. Otherwise test fails
-  #      RetryCommand -numberOfRetries 10 -command {(Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status} -expectedResult "Stopped"| Should be "Stopped"
-		##Act
-  #      # Try enabling Crawler Service
-  #      Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockEnableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName
-  #      {Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockEnableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName} | Should not Throw
-  #      #Assert
-  #      RetryCommand -numberOfRetries 10 -command {(Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status} -expectedResult "Running"| Should be "Running"
+        # Now Crawler Service should be fo sure disabled. Otherwise test fails
+        RetryCommand -numberOfRetries 10 -command {(Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status} -expectedResult "Stopped"| Should be "Stopped"
+		#Act
+        # Try enabling Crawler Service
+        Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockEnableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName
+        {Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockEnableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName} | Should not Throw
+        #Assert
+        RetryCommand -numberOfRetries 10 -command {(Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status} -expectedResult "Running"| Should be "Running"
 
-  #  }
+    }
     
-  #  It "Disable-ISHServiceCrawler disabled Crawler Service"{
-		##Arrange
-  #      # Make sure, that Crawler Service is disabled, otherwise - make it disabled 
-  #      $precondition = (Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status
-  #      if ($precondition -eq "Stopped"){Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockEnableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName }
+    It "Disable-ISHServiceCrawler disabled Crawler Service"{
+		#Arrange
+        # Make sure, that Crawler Service is disabled, otherwise - make it disabled 
+        $precondition = (Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status
+        if ($precondition -eq "Stopped"){Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockEnableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName }
 
-  #      # Now Crawler Service should be fo sure enabled. Otherwise test fails
-  #      RetryCommand -numberOfRetries 10 -command {(Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status} -expectedResult "Running"| Should be "Running"
-		##Act
-  #      # Try enabling Crawler Service
-  #      Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockDisableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName
-  #      {Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockDisableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName} | Should not Throw
-  #      RetryCommand -numberOfRetries 10 -command {(Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status} -expectedResult "Stopped"| Should be "Stopped"
-  #  }
+        # Now Crawler Service should be fo sure enabled. Otherwise test fails
+        RetryCommand -numberOfRetries 10 -command {(Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status} -expectedResult "Running"| Should be "Running"
+		#Act
+        # Try enabling Crawler Service
+        Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockDisableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName
+        {Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockDisableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName} | Should not Throw
+        RetryCommand -numberOfRetries 10 -command {(Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status} -expectedResult "Stopped"| Should be "Stopped"
+    }
     
-  #  It "Enable-ISHServiceCrawler changes component state"{
-		##Arrange
-  #      # Make sure, that Crawler Service is disabled, otherwise - make it disabled 
-  #      $precondition = (Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status
-  #      if ($precondition -eq "Running"){Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockDisableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName }
-  #      # Now Crawler Service should be fo sure disabled. Otherwise test fails
-  #      RetryCommand -numberOfRetries 10 -command {(Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status} -expectedResult "Stopped"| Should be "Stopped"
-		##Act
-  #      # Try enabling Crawler Service
-  #      Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockEnableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName
-  #      RetryCommand -numberOfRetries 10 -command {(Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status} -expectedResult "Running"| Should be "Running"
-  #      $comp = Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHComponent -Session $session -ArgumentList $testingDeploymentName
-  #      ($comp | Where-Object -Property Name -EQ "Crawler").IsEnabled | Should be "True"
-  #  }
+    It "Enable-ISHServiceCrawler changes component state"{
+		#Arrange
+        # Make sure, that Crawler Service is disabled, otherwise - make it disabled 
+        $precondition = (Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status
+        if ($precondition -eq "Running"){Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockDisableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName }
+        # Now Crawler Service should be fo sure disabled. Otherwise test fails
+        RetryCommand -numberOfRetries 10 -command {(Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status} -expectedResult "Stopped"| Should be "Stopped"
+		#Act
+        # Try enabling Crawler Service
+        Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockEnableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName
+        RetryCommand -numberOfRetries 10 -command {(Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName)[0].Status} -expectedResult "Running"| Should be "Running"
+        $comp = Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetISHComponent -Session $session -ArgumentList $testingDeploymentName
+        ($comp | Where-Object -Property Name -EQ "Crawler").IsEnabled | Should be "True"
+    }
      
-  #  It "Enable-ISHServiceCrawler writes history"{       
-  #      #Act
-  #      Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockEnableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName
-  #      $history = Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetHistory -Session $session -ArgumentList $testingDeploymentName
-  #      $history.Contains('Enable-ISHServiceCrawler -ISHDeployment') | Should be "True"
-  #  }
-  #  UndoDeploymentBackToVanila $testingDeploymentName $true
+    It "Enable-ISHServiceCrawler writes history"{       
+        #Act
+        Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockEnableISHServiceCrawler -Session $session -ArgumentList $testingDeploymentName
+        $history = Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockGetHistory -Session $session -ArgumentList $testingDeploymentName
+        $history.Contains('Enable-ISHServiceCrawler -ISHDeployment') | Should be "True"
+    }
+    UndoDeploymentBackToVanila $testingDeploymentName $true
 }

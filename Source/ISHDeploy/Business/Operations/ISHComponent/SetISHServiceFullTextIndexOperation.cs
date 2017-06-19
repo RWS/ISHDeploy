d24e9c7ae8f1859fabfd1145861c76dab639b6fc
vmaksymenko <vmaksymenko@sdl.com>
2017-06-19 17:56:56 +0300

    TS-12560 - Initial implementation

--- Source/ISHDeploy/Business/Operations/ISHComponent/SetISHServiceFullTextIndexOperation.cs
+++ Source/ISHDeploy/Business/Operations/ISHComponent/SetISHServiceFullTextIndexOperation.cs
@@ -55,6 +55,63 @@ namespace ISHDeploy.Business.Operations.ISHComponent
             Invoker.AddAction(new SetRegistryValueAction(logger, RegInfoShareAuthorRegistryElement, RegistryValueName.SolrLuceneBaseUrl, uri));
             Invoker.AddAction(new SetRegistryValueAction(logger, RegInfoShareBuildersRegistryElement, RegistryValueName.SolrLuceneBaseUrl, uri));
 
+            // Remove dependencies between Crawler and SolrLucene
+            var serviceManager = ObjectFactory.GetInstance<IWindowsServiceManager>();
+            var services = serviceManager.GetServices(ishDeployment.Name, ISHWindowsServiceType.Crawler);
+
+            foreach (var service in services)
+            {
+                Invoker.AddAction(new SetRegistryValueAction(logger, string.Format(RegWindowsServicesRegistryPathPattern, service.Name), RegistryValueName.DependOnService, string.Empty));
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SetISHServiceFullTextIndexOperation"/> class.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
+        /// <param name="ishDeployment">The instance of the deployment.</param>
+        /// <param name="port">The target lucene port.</param>
+        /// <param name="portRegistryValueName">The name of port value in registry.</param>
+        /// <param name="solrLuceneStopKey">The StopKey.</param>
+        public SetISHServiceFullTextIndexOperation(ILogger logger, Models.ISHDeployment ishDeployment, int port, RegistryValueName portRegistryValueName, string solrLuceneStopKey = "") :
+            base(logger, ishDeployment)
+        {
+
+            if (portRegistryValueName != RegistryValueName.SolrLuceneServicePort ||
+                portRegistryValueName != RegistryValueName.SolrLuceneStopPort)
+            {
+                throw new AggregateException($"Operation does not support such RegistryValueName value: {portRegistryValueName}");
+            }
+
+            Invoker = new ActionInvoker(logger, $"Setting of target lucene {portRegistryValueName} port of {ISHWindowsServiceType.SolrLucene} windows services");
+
+            // Make sure Vanilla backup of all windows services exists
+            Invoker.AddAction(new WindowsServiceVanillaBackUpAction(logger, VanillaPropertiesOfWindowsServicesFilePath, ishDeployment.Name));
+
+            Invoker.AddAction(new SetRegistryValueAction(logger, RegInfoShareBuildersRegistryElement, portRegistryValueName, port));
+
+            if (portRegistryValueName == RegistryValueName.SolrLuceneServicePort)
+            {
+                var registryManager = ObjectFactory.GetInstance<ITrisoftRegistryManager>();
+                var currentLuceneUri =
+                    new Uri(
+                        registryManager.GetRegistryValue(RegInfoShareBuildersRegistryElement,
+                            RegistryValueName.SolrLuceneBaseUrl).ToString());
+
+                var newUriAsString = currentLuceneUri.ToString().Replace(currentLuceneUri.Port.ToString(), port.ToString());
+                var newUri = new Uri(newUriAsString);
+
+                Invoker.AddAction(new SetRegistryValueAction(logger, RegInfoShareAuthorRegistryElement, RegistryValueName.SolrLuceneBaseUrl, newUri));
+                Invoker.AddAction(new SetRegistryValueAction(logger, RegInfoShareBuildersRegistryElement, RegistryValueName.SolrLuceneBaseUrl, newUri));
+            }
+            else
+            {
+                Invoker.AddAction(new SetRegistryValueAction(logger, RegInfoShareAuthorRegistryElement, RegistryValueName.SolrLuceneStopKey, solrLuceneStopKey));
+            }
+
+            // Open port
+
+            // Remove dependencies between Crawler and SolrLucene
             var serviceManager = ObjectFactory.GetInstance<IWindowsServiceManager>();
             var services = serviceManager.GetServices(ishDeployment.Name, ISHWindowsServiceType.Crawler);
 

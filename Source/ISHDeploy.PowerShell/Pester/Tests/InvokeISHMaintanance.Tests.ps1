191882359a0d3a0af5d747f2e6c919824fd03a4c
Denys Havryliuk <dhavryliuk@sdl.com>
2017-12-05 11:43:53 +0200

    added tests

--- Source/ISHDeploy.PowerShell/Pester/Tests/InvokeISHMaintanance.Tests.ps1
+++ Source/ISHDeploy.PowerShell/Pester/Tests/InvokeISHMaintanance.Tests.ps1
@@ -36,11 +36,36 @@ Describe "Testing ISHMaintanance"{
         #Act
         $debugMessage = Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockInvokeISHMaintanance -Session $session -ArgumentList $testingDeploymentName, $params
         #Arrange
-        $debugMessage -like "*Invalid commandline arguments: use*" | Should be $true
+        $debugMessage -like "*Invalid commandline arguments: use*" | Should be $false
+        $debugMessage -like "*Reindexing was completed for the current catalog*" | Should be $true
         $DebugPreference = $oldDebugPreference
     }
     
-    
+    It "Register Crawler gives successful result in crawler output"{
+
+		#Arrange
+        $oldDebugPreference = $DebugPreference
+        $DebugPreference = "Continue"
+        $params = @{Crawler = $true; Register = $true }
+        #Act
+        $debugMessage = Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockInvokeISHMaintanance -Session $session -ArgumentList $testingDeploymentName, $params
+        #Arrange
+        $debugMessage -like "*Registering the Crawler for * was successful*" | Should be $true
+        $DebugPreference = $oldDebugPreference
+    }
+
+    It "Unregister Crawler gives successful result in crawler output"{
+
+		#Arrange
+        $oldDebugPreference = $DebugPreference
+        $DebugPreference = "Continue"
+        $params = @{Crawler = $true; UnRegisterAll = $true }
+        #Act
+        $debugMessage = Invoke-CommandRemoteOrLocal -ScriptBlock $scriptBlockInvokeISHMaintanance -Session $session -ArgumentList $testingDeploymentName, $params
+        #Arrange
+        $debugMessage -like "All registered crawlers for * are successful unregistered.*" | Should be $true
+        $DebugPreference = $oldDebugPreference
+    }
 
     UndoDeploymentBackToVanila $testingDeploymentName $true
 }
